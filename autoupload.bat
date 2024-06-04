e5969d9844712da3f8ba425358247e98072f10f2
Farhan <screetname70@gmail.com>
2024-06-04 19:35:09 +0800

    Perubahan dilakukan oleh Pongo pada 04/06/2024 pukul 19:35:09,04

--- autoupload.bat
+++ autoupload.bat
@@ -1,4 +1,4 @@
 git remote add origin https://github.com/hannuxs/farhan.git
-git commit -m "%date% %time%"
+git commit -m "Perubahan dilakukan oleh %username% pada %date% pukul %time% "
 git add .
 git push origin main
